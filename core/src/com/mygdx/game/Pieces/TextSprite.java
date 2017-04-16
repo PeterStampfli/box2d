51a3ca28afa8568518d0bfecbe516c8ca476ab85
peter <pestampf@gmail.com>
2017-04-16 20:49:55 +0200

    Clipper more versatile, finished. NEXT: BigText

--- core/src/com/mygdx/game/Pieces/TextSprite.java
+++ core/src/com/mygdx/game/Pieces/TextSprite.java
@@ -5,22 +5,24 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Created by peter on 4/16/17.
  */
 
 public class TextSprite extends TouchableSprite {
-    static public GlyphLayout glyphLayout=new GlyphLayout();
+    static public GlyphLayout glyphLayout = new GlyphLayout();
     static public BitmapFont bitmapFont;
 
-    public String text="";
+    public String text = "";
 
     /**
      * creates a box2DSprite based on a Texture with same size
+     *
      * @param texture
      */
-    public TextSprite(Texture texture){
+    public TextSprite(Texture texture) {
         super(texture);
     }
 
@@ -30,38 +32,52 @@ public class TextSprite extends TouchableSprite {
      *
      * @param textureRegion
      */
-    public TextSprite(TextureRegion textureRegion){
+    public TextSprite(TextureRegion textureRegion) {
         super(textureRegion);
     }
 
     /**
      * set the bitmap font to use
+     *
      * @param bitmapFont
      */
-    static public void setBitmapFont(BitmapFont bitmapFont){
-        TextSprite.bitmapFont=bitmapFont;
+    static public void setBitmapFont(BitmapFont bitmapFont) {
+        TextSprite.bitmapFont = bitmapFont;
     }
 
     /**
-     * set the text of the sprite, initialized to an empty string
+     * set the text of the sprite, was initialized to an empty string
+     *
      * @param text
      * @return
      */
-    public TextSprite setText(String text){
-        this.text=text;
+    public TextSprite setText(String text) {
+        this.text = text;
         return this;
     }
 
     /**
      * draw the sprite shape and the text (without rotation)
+     *
      * @param batch
      */
     @Override
-    public void draw(Batch batch){
+    public void draw(Batch batch) {
         super.draw(batch);
-        glyphLayout.setText(bitmapFont,text);
-        bitmapFont.draw(batch,glyphLayout,getWorldOriginX()-glyphLayout.width*0.5f,
-                                          getWorldOriginY()+glyphLayout.height*0.5f);
+        glyphLayout.setText(bitmapFont, text);
+        bitmapFont.draw(batch, glyphLayout, getWorldOriginX() - glyphLayout.width * 0.5f,
+                                            getWorldOriginY() + glyphLayout.height * 0.5f);
     }
 
+    /**
+     * translate without rotation
+     * @param touchPosition
+     * @param deltaTouchPosition
+     * @return
+     */
+    @Override
+    public boolean touchDrag(Vector2 touchPosition, Vector2 deltaTouchPosition) {
+        translate(deltaTouchPosition.x, deltaTouchPosition.y);
+        return true;
+    }
 }
