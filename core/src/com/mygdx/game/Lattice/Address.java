8c5388887cb47573c64440b9850032bb995a3cb5
peter <pestampf@gmail.com>
2017-05-20 16:27:50 +0200

    address work continued

--- core/src/com/mygdx/game/Lattice/Address.java
+++ core/src/com/mygdx/game/Lattice/Address.java
@@ -1,5 +1,7 @@
 package com.mygdx.game.Lattice;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by peter on 5/20/17.
  */
@@ -36,6 +38,7 @@ public class Address {
 
     /**
      * set Address from position components and given lattice.
+     *
      * @param x
      * @param y
      * @return
@@ -46,5 +49,98 @@ public class Address {
         return this;
     }
 
+    /**
+     * set address from position vector and given lattice
+     *
+     * @param vector
+     * @return
+     */
+    public Address fromPosition(Vector2 vector){
+        fromPosition(vector.x,vector.y);
+        return this;
+    }
 
+    /**
+     * get x-component of vector to this address
+     */
+    public float getX(){
+        return lattice.getX(this);
+    }
+
+    /**
+     * get y-component of vector to this address
+     */
+    public float getY(){
+        return lattice.getY(this);
+    }
+
+    /**
+     * set position vector depending on this address and given lattice
+     * @param vector
+     * @return
+     */
+    public Address toPosition(Vector2 vector){
+        vector.x=lattice.getX(this);
+        vector.y=lattice.getY(this);
+        return this;
+    }
+
+    /**
+     * Check if the address is inside the lattice
+     *
+     * @return
+     */
+    public boolean isInside(){
+        return lattice.isInside(this.i,this.j);
+    }
+
+    /**
+     * Make a step upwards in the lattice
+     *
+     * @return
+     */
+    public Address stepUp(){
+        lattice.stepUp(this);
+        return this;
+    }
+
+    /**
+     * Make a step down in the lattice
+     *
+     * @return
+     */
+    public Address stepDown(){
+        lattice.stepDown(this);
+        return this;
+    }
+
+    /**
+     * Make a step right in the lattice
+     *
+     * @return
+     */
+    public Address stepRight(){
+        lattice.stepRight(this);
+        return this;
+    }
+
+    /**
+     * Make a step left in the lattice
+     *
+     * @return
+     */
+    public Address stepLeft(){
+        lattice.stepLeft(this);
+        return this;
+    }
+
+    /**
+     * Make a step up and right in the lattice
+     *
+     * @return
+     */
+    public Address stepUpRight(){
+        lattice.stepUpRight(this);
+        return this;
+    }
 }
