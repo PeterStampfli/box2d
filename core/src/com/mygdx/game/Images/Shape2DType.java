e17bc101edc2a9bf13c69879f048057c021758e7
peter <pestampf@gmail.com>
2017-08-14 07:27:56 +0200

    shape2DType

--- core/src/com/mygdx/game/Images/Shape2DType.java
+++ core/src/com/mygdx/game/Images/Shape2DType.java
@@ -1,8 +1,47 @@
 package com.mygdx.game.Images;
 
 /**
- * Created by peter on 8/13/17.
+ * the different shape types: for reading/writing as byte
  */
 
 public enum Shape2DType {
+    COLLECTION(0),
+    CIRCLE(1),
+    RECTANGLE(2),
+    POLYGON(3),
+    POLYLINE(4),
+    POLYPOINT(5),
+    EDGE(6),
+    CHAIN(7);
+
+    private int value;
+
+    Shape2DType(int value){
+        this.value=value;
+    }
+
+    /**
+     * get the byte value that represents the type
+     *
+     * @return
+     */
+    public byte toByte() {
+        return (byte) value;
+    }
+
+    /**
+     * from a byte value obtain the type
+     *
+     * @param i
+     * @return
+     */
+    public static Shape2DType ofByte(int i){
+        for (Shape2DType type:Shape2DType.values()){
+            if (type.toByte()==i){
+                return type;
+            }
+        }
+        return  null;
+    }
+
 }
