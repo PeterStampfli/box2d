e2384a65951cb5a7cef0669075697c5b8a00908c
peter <pestampf@gmail.com>
2017-09-24 21:03:12 +0200

    timer that does not interfere with box2d etc

---
+++ core/src/com/mygdx/game/utilities/Timer.java
@@ -0,0 +1,81 @@
+package com.mygdx.game.utilities;
+
+/**
+ * A timer that updates and acts explicitly in the render method ...
+ * Will not interfere with box2d physics step
+ */
+
+public class Timer {
+    Runnable event;
+    float timeSinceLastAction;
+    float timeInterval;
+    int remainingEvents;
+
+    /**
+     * create a new timer, everything is stopped
+     */
+    public Timer(Runnable event){
+        stop();
+        setEvent(event);
+    }
+
+    /**
+     * set the Runnable event
+     *
+     * @param event Runnable, to action caused by the timer
+     */
+    public void setEvent(Runnable event) {
+        this.event = event;
+    }
+
+    /**
+     * start the timer with new time interval and number of events
+     *
+     * @param timeInterval float, time between events in sec
+     * @param numberOfEvents int, number of events
+     */
+    public void start(float timeInterval,int numberOfEvents){
+        timeSinceLastAction=0;
+        this.timeInterval =timeInterval;
+        remainingEvents=numberOfEvents;
+    }
+
+    /**
+     * make a single delayed event
+     *
+     * @param timeInterval float, time delay for event
+     */
+    public void singleEvent(float timeInterval){
+        start(timeInterval,1);
+    }
+
+    /**
+     * repeat events forever
+     *
+     * @param timeInterval float, time delay for event
+     */
+    public void repeatForever(float timeInterval){
+        start(timeInterval,Integer.MAX_VALUE);
+    }
+
+    /**
+     * stop the timer. cancel events
+     */
+    public void stop(){
+        remainingEvents=0;
+    }
+
+    /**
+     * call in render-loop to advance time and do something
+     */
+    public void update(){
+        if (remainingEvents>0) {
+            timeSinceLastAction += TimeU.getTrueDeltaTime();
+            if (timeSinceLastAction> timeInterval){
+                timeSinceLastAction=0;
+                remainingEvents--;
+                event.run();
+            }
+        }
+    }
+}
